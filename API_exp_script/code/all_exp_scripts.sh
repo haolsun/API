86398717e6e5ced690d3a5c698be20dae1b697a1
Haoliang Sun <haolsun.cn@gmail.com>
2023-02-26 22:53:57 +0800

    Update all_exp_scripts.sh

--- API_exp_script/code/all_exp_scripts.sh
+++ API_exp_script/code/all_exp_scripts.sh
@@ -23,3 +23,7 @@ python train_val.py with dataset=FSS n_shots=1 batch_size=24
 python test.py with dataset=FSS mode=test load_snapshot='./runs/API_FSS_align_encoder_sets_0_1way_1shot_train/1/snapshots/best_val.pth'
 python train_val.py with dataset=FSS n_shots=5 batch_size=24
 python test.py with dataset=FSS mode=test load_snapshot='./runs/API_FSS_align_encoder_sets_0_1way_5shot_train/1/snapshots/best_val.pth'
+
+## for LIDC dataset
+python train_val.py with dataset=LIDC n_shots=1 batch_size=16
+python test.py with dataset=LIDC mode=test load_snapshot='./runs/API_LIDC_align_encoder_sets_0_1way_1shot_train/1/snapshots/best_val.pth'
